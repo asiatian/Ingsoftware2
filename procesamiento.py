2caec846923ea1a54b7a966aa808c6df94dfa463
asiatian <32916962+asiatian@users.noreply.github.com>
2018-12-05 19:03:56 -0300

    Update procesamiento.py

--- procesamiento.py
+++ procesamiento.py
@@ -1,10 +1,9 @@
-import sys
-import os
-import json
+import sys, os , json ,re
 import pandas as pd
-import re
+import nltk, unicodedata, inflect
 from datetime import datetime, timedelta
 from textblob import TextBlob
+from nltk.corpus import stopwords
 
 # # GLOBALES # #
 zoits = ["futaleufu","putre","osorno","valparaiso"]
@@ -68,12 +67,55 @@ def crear_bolsa(lugar,bolsa):
 	df = pd.DataFrame.from_dict(bolsa,orient='index')
 	df.to_csv('output/'+lugar+'_palabras.csv')
 
-def normalizar(texto):
+def remover_non_ascii(listaPalabras):
+	nueva_lista = []
+	for palabra in listaPalabras:
+		nueva_palabra = unicodedata.normalize('NFKD',palabra).encode('ascii','ignore').decode('utf-8','ignore')
+		nueva_lista.append(nueva_palabra)
+	return nueva_lista
+
+def remover_puntuacion(listaPalabras):
+	nueva_lista = []
+	for palabra in listaPalabras:
+		if(len(palabra)<13 and len(palabra)>3):
+			nueva_palabra = re.sub(r'http\S+|[^\w\s]|[_]','',palabra)
+			if nueva_palabra != '':
+				nueva_lista.append(nueva_palabra)
+	return nueva_lista
+
+def replace_numbers(listaPalabras):
+	p = inflect.engine()
+	nueva_lista = []
+	for palabra in listaPalabras:
+		if palabra.isdigit():
+			continue
+		else:
+			nueva_lista.append(palabra)
+	return nueva_lista
+
+def remove_stopwords(listaPalabras):
+	nueva_lista = []
+	for palabra in listaPalabras:
+		if(len(palabra)<13 and len(palabra)>3):
+			if palabra not in stopwords.words('spanish'):
+				nueva_lista.append(palabra)
+	return nueva_lista
+
+def normalizar(texto,bolsa):
 	texto = texto.lower()
-	listaPalabras = re.compile(r'\W+|\b[a-z]{1}\b|\b[a-z]{2}\b', re.UNICODE).split(texto)
-	for x in listaPalabras:
-		print x
-	return
+	listaPalabras = nltk.word_tokenize(texto)
+	listaPalabras = remover_non_ascii(listaPalabras)
+	listaPalabras = remover_puntuacion(listaPalabras)
+	listaPalabras = replace_numbers(listaPalabras)
+	listaPalabras = remove_stopwords(listaPalabras)
+	for palabra in listaPalabras:
+		if bolsa == None:
+			bolsa = {palabra : (1)}
+		elif palabra in bolsa.values():
+			bolsa[palabra] += 1
+		else:
+			bolsa[palabra] = (1)
+	return bolsa
 
 ## ............. ##
 
@@ -92,12 +134,10 @@ for lugar in zoits:
 				continue
 			else:
 				if(tweet['lang']=='es'):
-					normalizar(tweet['text'])
+					normalizar(tweet['text'],bolsa)
 					pos_contador,neu_contador,neg_contador=spanish_sentimiento(tweet['text'],pos_contador,neu_contador,neg_contador)
 				elif(tweet['lang']=='en'):
 					pos_contador,neu_contador,neg_contador=english_sentimiento(tweet['text'],pos_contador,neu_contador,neg_contador)
 				popular = buscar_popular(popular,tweet)
 		#crear_output(lugar,pos_contador,neu_contador,neg_contador,popular)
 		#crear_bolsa(lugar,bolsa)
-
-
